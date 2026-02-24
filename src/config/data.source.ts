71c68a5a5c371d2f789ef256bf71ec91a98307e3
shebin ks <shebinks01@gmail.com>
2026-02-24 13:12:05 +0530

    add db url

--- src/config/data.source.ts
+++ src/config/data.source.ts
@@ -4,14 +4,22 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const isDatabaseUrlAvailable = !!process.env.DATABASE_URL;
+
+
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 5432,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-
+    ...(isDatabaseUrlAvailable
+        ? {
+            url: process.env.DATABASE_URL,
+        }
+        : {
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT) || 5432,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+        }),
     synchronize: true,
     logging: false,
 
